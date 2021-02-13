613593296dfdca0fe8cb6b63cf259d5926f2385b
Nhv24 <69376044+Nhv24@users.noreply.github.com>
2021-02-13 16:22:47 -0700

    Add increment to box shadow

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,20 @@
+let n = 0;
+
 window.onscroll = () => {
+    // No scroll or scroll up
     if (window.scrollY <= 0)
     {
         document.querySelector('#navigation').style.boxShadow = 'none';
+        // Reset n
+        n = 0;
     }
     else {
-        document.querySelector('#navigation').style.boxShadow = '3px 3px 5px lightgray';
+        // If n is less than some number
+        if (n < 20)
+        {
+            document.querySelector('#navigation').style.boxShadow = `3px 3px ${n}px #2f4f4f`;
+            // Increment
+            n++;
+        }
     }
 }
