1036918df90705cca0c6cbc6806294534de59c0b
Nhv24 <69376044+Nhv24@users.noreply.github.com>
2021-02-18 15:16:36 -0700

    Implement copy

--- palette/palette.js
+++ palette/palette.js
@@ -1,6 +1,22 @@
+colors = [];
+
 function generate() {
     // Change the colors
     document.querySelectorAll('.clr').forEach(div => {
-        div.style.backgroundColor = '#' + Math.random().toString(16).substr(-6);;
+        let clr = '#' + Math.random().toString(16).substr(-6);
+        div.style.backgroundColor = clr;
+        colors.push(clr);
     });
+}
+
+function copy(n) {
+    if (colors[n] !== undefined)
+    {
+        var i = document.createElement("input");
+        document.body.appendChild(i);
+        i.value = colors[n];
+        i.select();
+        document.execCommand("copy");
+        document.body.removeChild(i);
+    }
 }
