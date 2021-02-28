761f0217ceae006fe331784afa1f819661399af6
Nhv24 <69376044+Nhv24@users.noreply.github.com>
2021-02-28 13:29:41 -0700

    Create avatar.js

---
+++ js/avatar.js
@@ -0,0 +1,11 @@
+function randomgimg(max = 5) {
+    var imgnum = Math.floor(Math.random() * max);
+
+    // Find the correct extension
+    if (imgnum === 0) {
+        return `avatar${imgnum}.jpg`;
+    }
+    else {
+        return `avatar${imgnum}.png`;
+    }
+}
