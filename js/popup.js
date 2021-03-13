72232945944bdff871ba6816d841e4db1f79eecd
Nhv24 <69376044+Nhv24@users.noreply.github.com>
2021-03-12 17:40:52 -0700

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,29 @@
-function popup(text) {
-    const screen_mask = $(".screen-mask");
+function setup() {
     // Set body to overflow: hidden
     $("body").css("overflow", "hidden");
 
     // Show the popup/alert using the popup's parent element
     $(".popup").parent().show();
+}
 
+function popup(text) {
+    setup();
     // Change the text
-	$(".popup-modal p").text(text);
+    $(".popup-modal p").text(text);
 
     // Close the modal when the button is clicked
     $(".popup-modal button").click(() => {
         $(".screen-mask").hide();
+    });
+}
+
+function conf(text) {
+    setup();
+
+    // Change the text
+    $(".confirm-modal").text(text);
+
+    $(".confirm-modal #cancel").click(() => {
+        return false;
     });
 }
