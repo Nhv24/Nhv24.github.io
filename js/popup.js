e17ddf52edf777ad0f88fc4ee6f2eb1578dc917e
Nhv24 <69376044+Nhv24@users.noreply.github.com>
2021-03-12 17:36:04 -0700

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -3,14 +3,14 @@ function popup(text) {
     // Set body to overflow: hidden
     $("body").css("overflow", "hidden");
 
-    // Show the popup/alert
-    screen_mask.show();
+    // Show the popup/alert using the popup's parent element
+    $(".popup").parent().show();
 
     // Change the text
 	$(".popup-modal p").text(text);
 
     // Close the modal when the button is clicked
     $(".popup-modal button").click(() => {
-        screen_mask.hide();
+        $(".screen-mask").hide();
     });
 }
